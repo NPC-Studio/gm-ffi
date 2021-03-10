560b99f7180eaee55dd123ba1add0ccce7acfd31
Gabe Weiner <imlazyeye@gmail.com>
2021-03-10 11:11:59 -0600

    Cleans up the str implementation and adds tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-use std::str::FromStr;
-
 /// A status code the represents the outcome of a Rust-side function,
 /// intended to be sent back to GameMaker.
 #[repr(transparent)]
@@ -33,18 +31,34 @@ impl GmPtr {
     /// # Saftey
     /// Assumes that the pointer being used is valid as a c_str pointer.
     pub fn to_str(self) -> Result<&'static str, std::str::Utf8Error> {
-        unsafe { cstr_core::CStr::from_ptr(self.0).to_str() }
-    }
-}
-impl FromStr for GmPtr {
-    type Err = cstr_core::NulError;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(GmPtr(cstr_core::CString::new(s)?.as_ptr()))
+        unsafe { cstr_core::CStr::from_ptr(self.0) }.to_str()
     }
 }
 impl core::ops::Deref for GmPtr {
     type Target = *const cty::c_char;
     fn deref(&self) -> &Self::Target {
         &self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn make_null_ptr() {
+        GmPtr::new(core::ptr::null());
+    }
+
+    #[test]
+    fn make_string_ptr() {
+        GmPtr::new("Hello, world!\0".as_ptr() as *const cty::c_char);
+    }
+
+    #[test]
+    fn read_string_ptr() {
+        let ptr = GmPtr::new("Hello, world!\0".as_ptr() as *const cty::c_char);
+        let out = ptr.to_str().unwrap();
+        assert_eq!(out, "Hello, world!");
     }
 }
